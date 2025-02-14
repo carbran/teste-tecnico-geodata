c70009bc67308392b86bb653e2a13cf90e028d41
Fernando Carlos Brandão Filho <brandao.fernando96@gmail.com>
2025-02-13 21:47:09 -0300

    :construction: criado migrations para tabelas projetos e tarefas

--- database/migrations/2025_02_14_001854_create_projetos.php
+++ database/migrations/2025_02_14_001854_create_projetos.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projetos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('titulo')->nullable(false);
+            $table->string('descricao');
+            $table->date('data_entrega');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projetos');
     }
 };
--- database/migrations/2025_02_14_001903_create_tarefas.php
+++ database/migrations/2025_02_14_001903_create_tarefas.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tarefas', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('descricao');
+            $table->integer('status')->default(0)->comment('0 = pendente, 1 = concluída / padrão: pendente');
+            $table->foreignId('id_projetos')->constrained('projetos');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tarefas');
     }
 };
